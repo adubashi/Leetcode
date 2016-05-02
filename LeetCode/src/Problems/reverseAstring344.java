499fc5ca953890e9b372d01e6f00108cd0038c44
adubashi <adubashi@gmail.com>
2016-05-02 13:32:47 -0700

    reverse a string

---
+++ LeetCode/src/Problems/reverseAstring344.java
@@ -0,0 +1,15 @@
+package Problems;
+
+public class reverseAstring344 {
+	public String reverseString(String s) {
+		StringBuilder st = new StringBuilder();
+		
+		for(int i = s.length() - 1; i >= 0; i--){
+			st.append(s.charAt(i));
+		}
+		
+		return st.toString();
+        
+    }
+
+}
