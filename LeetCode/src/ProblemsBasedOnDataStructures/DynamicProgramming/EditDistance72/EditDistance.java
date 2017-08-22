689c4fa156685c1c5787627500b0cd244a4be651
Arjun Dubashi <adubashi@gmail.com>
2017-08-21 22:49:28 -0700

    Edit Distance DP solution

--- LeetCode/src/ProblemsBasedOnDataStructures/DynamicProgramming/EditDistance72/EditDistance.java
+++ LeetCode/src/ProblemsBasedOnDataStructures/DynamicProgramming/EditDistance72/EditDistance.java
@@ -34,10 +34,8 @@ public class EditDistance {
 		}
 	}
 
-	
 	//Second Attempt 
-	//Uses two indexes --> one for each string 
-	
+	//Uses two indexes --> one for each string 	
 	public static int minDistance(String word1, String word2, int index1, int index2) {
 		//If word1 is 0, then we just need to get rid of all the letters in word2
 		if(index1 == 0) {
@@ -65,5 +63,39 @@ public class EditDistance {
 	
 	public static int min(int a, int b, int c) {
 		return Math.min(Math.min(a, b), c);
+	}
+	
+	public static int minDistanceDP(String word1, String word2) {
+		int n = word1.length();
+		int m = word2.length();
+		
+		int[][] dp = new int[n+1][m+1];
+		
+		for(int i = 0; i <= n; i++) {
+			for(int j = 0; j <= m; j++) {
+				if(i == 0) {
+					dp[i][j] = j;
+				}
+				if(j == 0) {
+					dp[i][j] = i;
+				}
+				
+				if(word1.charAt(i-1) == word2.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1];
+				} else {
+					dp[i][j] = 1 + 
+					  min(
+					  //Delete 
+					  dp[i-1][j],
+					  //Replace
+					  dp[i-1][j-1],
+					  //Add
+					  dp[i][j-1]			  
+					  );
+				}
+			}
+		}
+		
+		return dp[n][m];
 	}
 }
