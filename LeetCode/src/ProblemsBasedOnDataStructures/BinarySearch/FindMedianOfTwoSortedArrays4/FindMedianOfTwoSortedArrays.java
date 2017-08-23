fa9ce96026d87ba0fbe2d3fab2b364762430b692
Dubashi <dubashia@amazon.com>
2017-08-23 12:40:24 -0700

    Linear solution to find median of two sorted arrays

--- LeetCode/src/ProblemsBasedOnDataStructures/BinarySearch/FindMedianOfTwoSortedArrays4/FindMedianOfTwoSortedArrays.java
+++ LeetCode/src/ProblemsBasedOnDataStructures/BinarySearch/FindMedianOfTwoSortedArrays4/FindMedianOfTwoSortedArrays.java
@@ -1,6 +1,27 @@
 package ProblemsBasedOnDataStructures.BinarySearch.FindMedianOfTwoSortedArrays4;
 
 public class FindMedianOfTwoSortedArrays {
+	
+	
+	public static void main(String args[]) {
+		
+		
+		double[] array = {1,2,3};
+		double[] array2 = {4,5,6};
+		
+		
+		double ans = findMedianSortedArraysLinear(array,array2);
+		
+		
+		System.out.println(ans);
+		
+	}
+	
+	
+	
+	
+	
+	
 	/*
 	 * There are two sorted arrays nums1 and nums2 of size m and n respectively.
        Find the median of the two sorted arrays. 
@@ -8,9 +29,36 @@ public class FindMedianOfTwoSortedArrays {
 	 */
 	
 	
-	
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		return 0;
+	/*
+	 * 
+	 * Using two indices for each array and one index to track the total 
+	 * number of elements to produce a linear solution
+	 * 
+	 */
+	public static double findMedianSortedArraysLinear(double[] nums1, double[] nums2) {
+		int index1 = 0;
+		int index2 = 0;
+		
+		int totalIndex = (nums1.length + nums2.length)/2;
+		
+		
+		while(true) {
+			if(nums1[index1] < nums2[index2] ) {
+			   index1++;
+			   totalIndex--;
+			   if(totalIndex == 0) {
+				   return nums2[index2];
+			   }
+			} else {
+				index2++;
+				totalIndex--;
+				if(totalIndex == 0) {
+					return nums1[index1];
+				}
+			}
+		}
+		//Should not happen 
+		//return nums1[index1];
 	}
 
 }
