11bba5f6006c8327f14d873b67e9c9e5f17de64e
Arjun Dubashi <adubashi@gmail.com>
2017-08-29 22:57:53 -0700

    Find Median Linear

--- LeetCode/src/ProblemsBasedOnDataStructures/BinarySearch/FindMedianOfTwoSortedArrays4/FindMedianOfTwoSortedArrays.java
+++ LeetCode/src/ProblemsBasedOnDataStructures/BinarySearch/FindMedianOfTwoSortedArrays4/FindMedianOfTwoSortedArrays.java
@@ -34,31 +34,57 @@ public class FindMedianOfTwoSortedArrays {
 	 * Using two indices for each array and one index to track the total 
 	 * number of elements to produce a linear solution
 	 * 
+	 * 
+	 * 
+	 * 
+	 * The naive implementation resolves to linear time. 
+	 * As we are given arrays we know the length of each in constant time and therefore know where the median should resolve.
+	 *  We loop through the index of our median (med_idx), at each stage 
+	 *  we maintain our previous value m1. m2 is updated in our logic block.
+	 *  If we've traversed array a completely, we know we must continue traversing b,
+	 *  and vice versa. If we have not traversed either 
+	 *  completely we simply check for the lower value and 
+	 *  increment pointer i (corresponding to array a) or j (corresponding to array b).
 	 */
 	public static double findMedianSortedArraysLinear(double[] nums1, double[] nums2) {
 		int index1 = 0;
 		int index2 = 0;
 		
-		int totalIndex = (nums1.length + nums2.length)/2;
+		double element1 = 0;
+		double element2 = 0;
 		
 		
-		while(true) {
-			if(nums1[index1] < nums2[index2] ) {
-			   index1++;
-			   totalIndex--;
-			   if(totalIndex == 0) {
-				   return nums2[index2];
-			   }
+		
+		int totalLength = (nums1.length + nums2.length) / 2;
+		
+		int tracker = 0;
+		
+		while(tracker < totalLength + 1) {
+			element1 = element2;
+			//If we hit the end of array
+			if(index1 == nums1.length) {
+			   element2 = nums2[index2];
+			   index2++;
+			} else if(index2 == nums2.length) {
+				element2 = nums1[index1];
+				index1++;
 			} else {
-				index2++;
-				totalIndex--;
-				if(totalIndex == 0) {
-					return nums1[index1];
-				}
+				if(nums1[index1] < nums2[index2]) {
+					element2 = nums1[index1];
+					index1++;
+				} else {
+					element2 = nums2[index2];
+					index2++;
+				}	
 			}
+			tracker++;
 		}
-		//Should not happen 
-		//return nums1[index1];
+		
+		if((nums1.length + nums2.length) % 2 == 0) {
+			return (element1 + element2)/2; 
+		} else {
+			return element2;
+		}
 	}
 
 }
