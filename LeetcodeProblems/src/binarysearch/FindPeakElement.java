8f238cf7880b12f14ed87d6a1c36bb31e753d5ee
Dubashi <dubashia@amazon.com>
2019-03-11 09:04:56 -0700

    Peak Element

---
+++ LeetcodeProblems/src/binarysearch/FindPeakElement.java
@@ -0,0 +1,14 @@
+package binarysearch;
+
+public class FindPeakElement {
+	
+	public static void main(String args[]) {
+		int[] array = {1,2,3,1};
+	}
+	
+	
+	public static int findPeakElement(int[] nums) {
+        
+    }
+
+}
